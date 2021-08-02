cf91c4234e390b130ff8ef9f27a88704aad698c9
Alvaro <alvaro.castillo777@gmail.com>
2021-08-02 02:59:56 -0500

    Adding the whole app

---
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "temperature-converter",
+  templateUrl: "./temperatureConverter.component.html",
+  styleUrls: ["./temperatureConverter.component.scss"],
+})
+export class TemperatureConverter implements OnInit {
+
+
+  constructor() {}
+
+  ngOnInit() {}
+
+
+}
