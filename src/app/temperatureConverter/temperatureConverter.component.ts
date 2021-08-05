ef9f1234be1afb5e4d3fced411a2a3643e3efef8
Abraham Griffin <abraham.griffin@ttec.com>
2021-08-04 19:08:00 -0500

    Feat() Create logic with formula for Celsius and Farenheit conversor

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,16 +1,33 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
-  selector: "temperature-converter",
-  templateUrl: "./temperatureConverter.component.html",
-  styleUrls: ["./temperatureConverter.component.scss"],
+  selector: 'temperature-converter',
+  templateUrl: './temperatureConverter.component.html',
+  styleUrls: ['./temperatureConverter.component.scss'],
 })
-export class TemperatureConverter implements OnInit {
+export class TemperatureConverter {
+  celsius: any //Declare my variables with type Any.
+  farenh: any
 
-
-  constructor() {}
-
-  ngOnInit() {}
-
-
+  ngOnInit() {
+    // Celsius = (F − 32) × 5/9
+    // Farenheit = C*9/5 + 32
+  }
+  changed(value: any, type: any) {
+    // Create the changemodel to accept the event and the value from the input
+    if (value) {
+      //If value has data will be true and will enter to the statement / if vale is empty will go to the else statement to empty the inputs.
+      if (type === 'celsius') {
+        //Create if/else statement and implement the given formula.
+        let farenheit = (value * 9) / 5 + 32
+        this.farenh = farenheit.toFixed(1)
+      } else {
+        let cels = (value - 32) * (5 / 9)
+        this.celsius = cels.toFixed(1)
+      }
+    } else {
+      this.celsius = null
+      this.farenh = null
+    }
+  }
 }
