b3789b5dccde032923214514112aab2f6a618b93
Giovanni Manjarrez Montelongo <giomontelongo@gmail.com>
2022-03-07 16:17:46 -0700

    Giovanni Manjarrez - Solucion

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { Temperature } from './temperature';
 
 @Component({
   selector: "temperature-converter",
@@ -7,8 +8,34 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class TemperatureConverter implements OnInit {
 
+  public temperature: Temperature;
 
-  constructor() {}
+  constructor() {
+    this.temperature = {
+      celsius : null,
+      fahrenheit : null
+    };
+  }
+
+  toConvert(tipo){
+
+    if(tipo){
+      if(this.temperature.celsius == null){
+        this.temperature.fahrenheit = null;
+      }
+      else{
+        this.temperature.fahrenheit =   Math.round( (this.temperature.celsius * 9 / 5 + 32) * 10 ) / 10;
+      }
+    }
+    else{
+      if(this.temperature.fahrenheit == null){
+        this.temperature.celsius = null;
+      }
+      else{
+        this.temperature.celsius =  Math.round( ( (this.temperature.fahrenheit - 32) * 5/9 ) * 10 ) / 10;
+      }
+    }
+  }
 
   ngOnInit() {}
 
